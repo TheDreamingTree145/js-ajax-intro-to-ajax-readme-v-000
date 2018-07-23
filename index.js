83ab9cb34a8c25dc684ba7d9dfb98bc0771a8597
Dustin Anderson <dandersonsoftware@outlook.com>
2018-07-23 19:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function showRepositories(event, data) {
+  debugger;
   let repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" date-repo" ' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
